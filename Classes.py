394bb2ad9fabb405011e4b9b6acfc5360727672e
JL Cox <jameslivingstoncox@gmail.com>
2019-11-09 13:23:01 -0600

    Completed migrating style variables to tkinter.ttk.Style()

--- Classes.py
+++ Classes.py
@@ -4,9 +4,9 @@ from pyspectator.network import NetworkInterface # Library for access resource s
 from pythonping import ping # Library used for the simplified running of network commands
 import re # Regex for detecting patterns in strings
 import subprocess # Library for running terminal commands through Python
-from time import sleep # Pauses code exection. Time passed to method in "seconds"
 import tkinter as tk # Library used to create the GUI and its widgets
 from tkinter import *
+import tkinter.ttk as ttk
 from tkinter.ttk import *
         
 mycolor = "#000000"
@@ -26,44 +26,76 @@ mycolor2 = "#430040"
 # 3) Add Menu system with options
 
 
-def set_app_style():
-    style = ttk.Style()
-    style.theme_create()
-    style.theme_use()
-
-
-class MainWindow(tk.Frame):
+class MainWindow():
     
     def __init__(self, master):
         
         # Object Variables
         self.last_ip = ""
-        self.ping_lat_list = []
+        self.pingLatList = []
         self.sleepTime = 1000
         self.isStopped = False
         
         # Create Window Elements
         self.master = master
-        self.build_master_window(self.master)
-        self.positioning_frames(self.master)
-        self.widgets(self.master)
-        self.menus(self.master)
+        self.build_master_window()
+        self.positioning_frames()
+        self.widgets()
+        self.menus()
         self.master.after(self.sleepTime, self.run)
         self.master.mainloop()
         
-    def build_master_window(self, master):
-        self.master.tk_setPalette(background=mycolor, foreground="black", activeBackground="white", activeForeground="black")
+    def build_master_window(self):
+        #self.master.tk_setPalette(background=mycolor, foreground="black", activeBackground="white", activeForeground="black")
+        self.style = ttk.Style()
+        self.style.theme_create("app_style", parent="alt",settings={
+        ".":             {"configure": {"background"      : "dim grey",
+                                        "foreground"      : "light grey",
+                                        "relief"          : "flat",
+                                        "highlightcolor"  : "spring green"}},
+        
+        "TFrame":             {"configure": {"background"      : "black",
+                                        "foreground"      : "black",
+                                        "relief"          : "flat",
+                                        "highlightcolor"  : "spring green"}},
+
+        "TLabel":        {"configure": {"foreground"      : "spring green",
+                                        "background"      : "black",
+                                        "padding"         : 0,
+                                        "font"            : ("Calibri", 12)}},
+
+        "TNotebook":     {"configure": {"padding"         : 5}},
+        "TNotebook.Tab": {"configure": {"padding"         : [25, 5], 
+                                        "foreground"      : "white"},
+                            "map"      : {"background"      : [("selected", "slate gray")],
+                                        "expand"          : [("selected", [1, 1, 1, 0])]}},
+
+        "TCombobox":     {"configure": {"selectbackground": "dim grey",
+                                        "fieldbackground" : "white",
+                                        "background"      : "light grey",
+                                        "foreground"      : "black"}},
+
+        "TButton":       {"configure": {"font"            :("Calibri", 13, 'bold'),
+                                        "background"      : "black",
+                                        "foreground"      : "red"},
+                            "map"      : {"background"      : [("active", "spring green")],
+                                        "foreground"      : [("active", 'black')]}},
+            
+        "TEntry":        {"configure": {"foreground"      : "black"}},
+        "Horizontal.TProgressbar":{"configure": {"background": "slate gray"}}
+        })
+        self.style.theme_use("app_style")
         self.master.geometry("500x165")
         self.master.title("SystemStatistics")
         
-    def positioning_frames(self, master):
-        self.main_container = Frame(master)
-        self.top_frame = Frame(self.main_container)
-        self.bottom_frame = Frame(self.main_container)
-        self.top_left = Frame(self.top_frame)
-        self.top_right = Frame(self.top_frame)
-        self.bottom_left = Frame(self.bottom_frame)
-        self.bottom_right = Frame(self.bottom_frame)
+    def positioning_frames(self):
+        self.main_container = ttk.Frame(self.master, style="TFrame")
+        self.top_frame = ttk.Frame(self.main_container, style="TFrame")
+        self.bottom_frame = ttk.Frame(self.main_container, style="TFrame")
+        self.top_left = ttk.Frame(self.top_frame, style="TFrame")
+        self.top_right = ttk.Frame(self.top_frame, style="TFrame")
+        self.bottom_left = ttk.Frame(self.bottom_frame, style="TFrame")
+        self.bottom_right = ttk.Frame(self.bottom_frame, style="TFrame")
         self.main_container.pack(side="top", fill="both", expand=True)
         self.top_frame.pack(side="top", fill="x", expand=False)
         self.bottom_frame.pack(side="bottom", fill="both", expand=True)
@@ -72,17 +104,17 @@ class MainWindow(tk.Frame):
         self.bottom_left.pack(side="left", fill="x", expand=True)
         self.bottom_right.pack(side="right", fill="x", expand=True)
      
-    def widgets(self, master):
-        self.cpu_temp = tk.Label(self.top_left, text="CPUTemp:", font="Helvetica 14 bold")
-        self.cpu_load = tk.Label(self.top_left, text="CPULoad:", font="Helvetica 14 bold", justify="left")
-        self.memory_stats = tk.Label(self.top_left, text="RAM:", font="Helvetica 14 bold", justify="left")
-        self.network_status = tk.Label(self.top_right, text="NetStatus:", font="Helvetica 14 bold", justify="right")
-        self.ip = tk.Label(self.master, text="IP:")
-        self.mac = tk.Label(self.master, text="MAC:")
-        self.close_button = tk.Button(self.master, command=self.master.destroy, fg="red", text="Close")
+    def widgets(self):
+        self.cpu_temp = ttk.Label(self.top_left, text="CPUTemp:", font="Helvetica 14 bold")
+        self.cpu_load = ttk.Label(self.top_left, text="CPULoad:", font="Helvetica 14 bold", justify="left")
+        self.memory_stats = ttk.Label(self.top_left, text="RAM:", font="Helvetica 14 bold", justify="left")
+        self.network_status = ttk.Label(self.top_right, text="NetStatus:", font="Helvetica 14 bold", justify="right")
+        self.ip = ttk.Label(self.bottom_frame, text="IP:")
+        self.mac = ttk.Label(self.bottom_frame, text="MAC:")
+        self.close_button = ttk.Button(self.bottom_frame, command=self.master.destroy, text="Close",style="TButton")
         
-    def menus(self, master):
-        self.menu01 = tk.Menu(self.master,fg="white", bg="grey")
+    def menus(self):
+        self.menu01 = tk.Menu(self.master)
         self.submenu01 = tk.Menu(self.menu01, tearoff=False)
         self.submenu01.add_command(label="Options",command=self.OpenOptionsMenu)
         self.menu01.add_cascade(label="File", menu=self.submenu01)
@@ -114,64 +146,64 @@ class MainWindow(tk.Frame):
         # CPU Temp
         _cpu_temp = float(self.find_cpu_temp())
         if _cpu_temp < 70:
-            self.cpu_temp["fg"] = "green"
+            self.cpu_temp["foreground"] = "green"
             self.cpu_temp["text"] = f"CPUTemp: {_cpu_temp}C"
         elif _cpu_temp >= 71 and _cpu_temp < 85:
-            self.cpu_temp["fg"] = "orange"
+            self.cpu_temp["foreground"] = "orange"
             self.cpu_temp["text"] = f"CPUTemp: {_cpu_temp}C"
         else:
-            self.cpu_temp["fg"] = "red"
+            self.cpu_temp["foreground"] = "red"
             self.cpu_temp["text"] = f"CPUTemp: {_cpu_temp}C"
         
         # CPU Load
         _cpu_load = self.find_cpu_load()
         if _cpu_load < 50:
-            self.cpu_load["fg"] = "green"
+            self.cpu_load["foreground"] = "green"
             self.cpu_load["text"] = f"CPULoad: {_cpu_load}%"
         elif _cpu_load >= 50 and _cpu_load < 80:
-            self.cpu_load["fg"] = "orange"
+            self.cpu_load["foreground"] = "orange"
             self.cpu_load["text"] = f"CPULoad: {_cpu_load}%"
         else:
-            self.cpu_load["fg"] = "red"
+            self.cpu_load["foreground"] = "red"
             self.cpu_load["text"] = f"CPULoad: {_cpu_load}%"
         
         # Memory
         mem = self.find_memory_stats()
         if mem.percent < 50:
-            self.memory_stats["fg"] = "green"
+            self.memory_stats["foreground"] = "green"
             self.memory_stats["text"] = f"RAM: {int(mem.used/1000000)}MB of {int(mem.total/1000000)}MB"
         elif mem.percent >= 50 and mem.percent < 80:
-            self.memory_stats["fg"] = "orange"
+            self.memory_stats["foreground"] = "orange"
             self.memory_stats["text"] = f"RAM: {int(mem.used/1000000)}MB of {int(mem.total/1000000)}MB"
         else:
-            self.memory_stats["fg"] = "red"
+            self.memory_stats["foreground"] = "red"
             self.memory_stats["text"] = f"RAM: {int(mem.used/1000000)}MB of {int(mem.total/1000000)}MB"        
         
         # Network Status
         try:
             net_results = self.check_network_status()
             if net_results[1] == 4:
-                self.network_status["fg"] = "green"
+                self.network_status["foreground"] = "green"
                 self.network_status["text"] = f"NetStatus: Good\nLatency: Avg({net_results[0]}ms)\nMax({net_results[3]}ms)\nMin({net_results[4]}ms)"
             elif net_results[1] < 4 and net_results[1] > 0:
-                self.network_status["fg"] = "orange"
+                self.network_status["foreground"] = "orange"
                 self.network_status["text"] = f"NetStatus: Poor\nLatency: Avg({net_results[0]}ms)\nMax({net_results[3]}ms)\nMin({net_results[4]}ms)"
             else:
-                self.network_status["fg"] = "red"
+                self.network_status["foreground"] = "red"
                 self.network_status["text"] = f"NetStatus: '8.8.8.8' is unreachable."
         except OSError:
-            self.network_status["fg"] = "red"
+            self.network_status["foreground"] = "red"
             self.network_status["text"] = f"NetStatus: Failed"
         
         # IP Address
         current_ip = self.find_network_ip()
         if current_ip != self.last_ip:
             last_ip = current_ip
-            self.ip["fg"] = "white"
+            self.ip["foreground"] = "white"
             self.ip["text"] = f"IP: {last_ip}"
         
         # MAC Address
-        self.mac["fg"] = "white"
+        self.mac["foreground"] = "white"
         self.mac["text"] = f"MAC: {self.find_network_mac()}"
         self.master.update()
         
@@ -197,20 +229,20 @@ class MainWindow(tk.Frame):
         
         for i in range(len(pings)):
             if pings[i][1] == True:
-                self.ping_lat_list.append(float(pings[i][0]))
+                self.pingLatList.append(float(pings[i][0]))
                 successes += 1
             else:
                 failures += 1
         
         if successes > 0:
-            latency = "%.2f" % (sum(self.ping_lat_list)/float(len(self.ping_lat_list)))
-            if len(self.ping_lat_list) > 100:
-                    _min = min(self.ping_lat_list)
-                    _max = max(self.ping_lat_list)
-                    self.ping_lat_list = self.ping_lat_list[:49]
-                    self.ping_lat_list.append(_min)
-                    self.ping_lat_list.append(_max)
-            return (float(latency),successes,failures,max(self.ping_lat_list),min(self.ping_lat_list))
+            latency = "%.2f" % (sum(self.pingLatList)/float(len(self.pingLatList)))
+            if len(self.pingLatList) > 100:
+                _min = min(self.pingLatList)
+                _max = max(self.pingLatList)
+                self.pingLatList = self.pingLatList[:49]
+                self.pingLatList.append(_min)
+                self.pingLatList.append(_max)
+            return (float(latency),successes,failures,max(self.pingLatList),min(self.pingLatList))
         else:
             return (0,successes,failures)
         
